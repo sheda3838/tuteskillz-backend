ea401ed643f706bf73a766b63f456e20117ba76e
Zaid Kamil <kamilzaid53@gmail.com>
2026-01-28 22:36:56 +0530

    Backend changed

--- config/db.js
+++ config/db.js
@@ -10,10 +10,10 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    ca: fs.readFileSync(process.env.CA)
+  // ssl: {
+  //   ca: fs.readFileSync(process.env.CA)
 
-  }
+  // }
 });
 
 db.connect((err) => {
