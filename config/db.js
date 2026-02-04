fe63f4a7d57fa6d58de17ddfbc4f67529747e233
Zaid Kamil <kamilzaid53@gmail.com>
2026-02-04 10:17:11 +0530

    Config file db connection line un commented

--- config/db.js
+++ config/db.js
@@ -1,19 +1,18 @@
 import mysql from "mysql2";
-import dotenv from 'dotenv'
-import fs from 'fs'
+import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST, 
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  // ssl: {
-  //   ca: fs.readFileSync(process.env.CA)
-
-  // }
+  ssl: {
+    ca: fs.readFileSync(process.env.CA),
+  },
 });
 
 db.connect((err) => {
